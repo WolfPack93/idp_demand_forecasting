2068fc209fbd3adbdc1bb491fe0a1c50c1fdd9f9
ewolfjr2-sdi <edward.wolf@searchdiscovery.com>
2024-06-03 18:57:25 -0400

    updated prophet python

--- prophet_forecast.py
+++ prophet_forecast.py
@@ -1,92 +1,104 @@
+import pandas_gbq
 import pandas as pd
-from prophet import Prophet
 import matplotlib.pyplot as plt
+from prophet import Prophet
 from prophet.diagnostics import cross_validation
 from prophet.diagnostics import performance_metrics
 from prophet.plot import plot_cross_validation_metric
 from google.oauth2 import service_account
-import pandas_gbq
 
+# Assign variables for GCP project and credentials
 gcp_project_id = "dce-gcp-training"
 
 credentials = service_account.Credentials.from_service_account_file(
     '.config/gcp_service_account.json',
 )
 
-# Load csv file
+# Load data from BigQuery
 data = pandas_gbq.read_gbq("SELECT * FROM `dce-gcp-training.idp_demand_forecasting.model_features`", project_id=gcp_project_id, credentials=credentials)
 
-# Initialize an empty DataFrame to store forecast data
-forecast_data = pd.DataFrame()
+# Convert date string to actual date datatype
+data['ds'] = pd.to_datetime(data['ds'])
 
-# Initialize an empty DataFrame to store cross-validation results
-cv_results_all = pd.DataFrame()
+# Data basic info
+print('== Data Info:')
+print(data.info())
+print(data.head())
 
-# Iterate over each distribution center
-for center_id, group in data.groupby('distribution_center_id'):
-    # 'ds' is your date column and 'y' is your demand column
-    # group = group.rename(columns={'ds': 'ds', 'y': 'y'})
+# One-hot encode categorical regressors
+data = pd.get_dummies(data, columns=['distribution_center_name', 'product_name'])
 
-    # Create and fit Prophet model
-    model = Prophet(yearly_seasonality=True
-                    , daily_seasonality=False)
-    model.fit(group)
+# Create Prophet model
+model = Prophet(
+    yearly_seasonality=True
+)
 
-    # Make future dates DataFrame for forecasting
-    future = model.make_future_dataframe(periods=30)  # Adjust forecast period as needed
+# Add regressors
+for col in data.columns:
+    if col.startswith('distribution_center_name_') or col.startswith('product_name_'):
+        model.add_regressor(col)
 
-    # Generate forecast for future dates
-    forecast = model.predict(future)
+# Fit model
+model.fit(data)
 
-    # Add distribution_center_id and inventory_item_id column to the forecast DataFrame
-    forecast['distribution_center_id'] = center_id
-    data['ds'] = pd.to_datetime(data['ds'])
-    forecast = forecast.merge(data, on=['distribution_center_id', 'ds'], how='left')
-    # print(forecast)
+# Make future dates DataFrame for forecasting
+future = model.make_future_dataframe(periods=30, freq='1D')  # Adjust forecast period as needed
 
-    forecast.rename(columns={'ds_x': 'ds'}, inplace=True)
-    # Concatenate the current forecast with existing forecast_data
-    forecast_data = pd.concat(
-        [forecast_data, forecast[['ds', 'distribution_center_id', 'product_id', 'yhat', 'yhat_lower', 'yhat_upper']]])
+# Get the last known values of the regressors
+last_known_values = data.iloc[-1][['distribution_center_name', 'product_name']]
 
-    # Perform cross-validation for the current distribution center
-    cv_results = cross_validation(model, initial='365 days', period='180 days', horizon='30 days')
+# Add regressors to the future dataframe
+future = future.merge(data[['ds', 'distribution_center_name', 'product_name']], on='ds', how='left')
 
-    # Add distribution_center_id to the cross-validation results
-    cv_results['distribution_center_id'] = center_id
+# Fill NaN values with the last known values
+future['distribution_center_name'].fillna(last_known_values['distribution_center_name'], inplace=True)
+future['product_name'].fillna(last_known_values['product_name'], inplace=True)
 
-    # Concatenate the current cross-validation results with existing cv_results_all
-    cv_results_all = pd.concat([cv_results_all, cv_results])
+# Check future dataframe output
+print('== Future Dataframe')
+print(future.info())
+print(future.head())
 
-# Reset index
-forecast_data.reset_index(drop=True, inplace=True)
-cv_results_all.reset_index(drop=True, inplace=True)
+# Generate forecast for future dates
+forecast = model.predict(future)
+
+forecast = forecast[['ds', 'distribution_center_name', 'product_name', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+# Check forecast dataframe output
+print('== Forecast Dataframe:')
+print(forecast.info())
+print(forecast.tail())
+
+# Perform cross-validation for the current distribution center
+cv_results = cross_validation(model, initial='730 days', period='90 days', horizon='30 days')
+
+# Print cv results
+print("== Cross-validation Results:")
+print(cv_results)
+
+# Visualize results
+plot_cross_validation_metric(cv_results, metric='mae')
+plt.show()
+
+# Calculate error metrics for all cross-validation results
+metrics = performance_metrics(cv_results)
+
+# Print error metrics
+print("== Performance Metrics:")
+print(metrics)
 
 # Output forecast data to a CSV file
 # forecast_data.to_csv('forecast_results.csv', index=False)
 
-# Example usage:
-print(forecast_data)
-
-# Calculate error metrics for all cross-validation results
-metrics = performance_metrics(cv_results_all)
-
-# Print error metrics
-print(metrics)
-
-# Visualize results
-# plot_cross_validation_metric(cv_results_all, metric='mae')
-# plt.show()
-
 # Push results to BigQuery
 
 # Forecast results
-pandas_gbq.to_gbq(
-    forecast_data, 'dce-gcp-training.idp_demand_forecasting.prophet_model_forecast_results', project_id=gcp_project_id, if_exists='replace',
-)
-
-# Model metrics
-metrics['horizon'] = metrics['horizon'].astype(str)
-pandas_gbq.to_gbq(
-    metrics, 'dce-gcp-training.idp_demand_forecasting.prophet_model_metrics', project_id=gcp_project_id, if_exists='replace',
-)
+# pandas_gbq.to_gbq(
+#     forecast, 'dce-gcp-training.idp_demand_forecasting.prophet_model_forecast_results', project_id=gcp_project_id, if_exists='replace',
+# )
+#
+# # Model metrics
+# metrics['horizon'] = metrics['horizon'].astype(str)
+# pandas_gbq.to_gbq(
+#     metrics, 'dce-gcp-training.idp_demand_forecasting.prophet_model_metrics', project_id=gcp_project_id, if_exists='replace',
+# )
